7da0311a18041da018b4f8a145165b63ed8ba43d
Edward Ryzhkov <85837973+bigsurmoon@users.noreply.github.com>
2022-06-03 08:51:23 +0700

    Update AuthTest.java

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -25,39 +25,39 @@ public class AuthTest {
     @Test
     void shouldUseActiveUser() {
         RegistrationInfo activeUser = user.setActiveUser();
-        $("[data-test-id='login'] input").sendKeys(activeUser.getLogin());
-        $("[data-test-id='password'] input").sendKeys(activeUser.getPassword());
-        $("[data-test-id='action-login']").click();
+        $("[data-test-id = 'login'] input").sendKeys(activeUser.getLogin());
+        $("[data-test-id = 'password'] input").sendKeys(activeUser.getPassword());
+        $("[data-test-id = 'action-login']").click();
         $(byText("Личный кабинет")).shouldBe(visible);
     }
 
     @Test
     void shouldUseBlockedUser() {
         RegistrationInfo blockedUser = user.setBlockedUser();
-        $("[data-test-id='login'] input").sendKeys(blockedUser.getLogin());
-        $("[data-test-id='password'] input").sendKeys(blockedUser.getPassword());
-        $("[data-test-id='action-login']").click();
-        $("[data-test-id='error-notification'] .notification__content")
+        $("[data-test-id = 'login'] input").sendKeys(blockedUser.getLogin());
+        $("[data-test-id = 'password'] input").sendKeys(blockedUser.getPassword());
+        $("[data-test-id = 'action-login']").click();
+        $("[data-test-id = 'error-notification'] .notification__content")
                 .shouldBe(visible).shouldHave(text("Пользователь заблокирован"));
     }
 
     @Test
     void shouldUseInvalidLogin() {
         RegistrationInfo activeUser = user.setActiveUser();
-        $("[data-test-id='login'] input").sendKeys(faker.name().username());
-        $("[data-test-id='password'] input").sendKeys(activeUser.getPassword());
-        $("[data-test-id='action-login']").click();
-        $("[data-test-id='error-notification'] .notification__content")
+        $("[data-test-id = 'login'] input").sendKeys(faker.name().username());
+        $("[data-test-id = 'password'] input").sendKeys(activeUser.getPassword());
+        $("[data-test-id = 'action-login']").click();
+        $("[data-test-id = 'error-notification'] .notification__content")
                 .shouldBe(visible).shouldHave(text("Неверно указан логин или пароль"));
     }
 
     @Test
     void shouldUseInvalidPassword() {
         RegistrationInfo activeUser = user.setActiveUser();
-        $("[data-test-id='login'] input").sendKeys(activeUser.getLogin());
-        $("[data-test-id='password'] input").sendKeys(faker.internet().password());
-        $("[data-test-id='action-login']").click();
-        $("[data-test-id='error-notification'] .notification__content")
+        $("[data-test-id = 'login'] input").sendKeys(activeUser.getLogin());
+        $("[data-test-id = 'password'] input").sendKeys(faker.internet().password());
+        $("[data-test-id = 'action-login']").click();
+        $("[data-test-id = 'error-notification'] .notification__content")
                 .shouldBe(visible).shouldHave(text("Неверно указан логин или пароль"));
     }
 }
